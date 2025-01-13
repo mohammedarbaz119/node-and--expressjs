54aebdb4e0454b3df602b7dcd7a435bdba921496
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2025-01-13 21:31:44 +0530

    feat: Added feature for adding products

--- expressjs/express.js
+++ expressjs/express.js
@@ -1,13 +1,30 @@
 const {readFileSync} = require('fs')
 const express = require('express')
 const path = require('path')
+const exp = require('constants')
 
 const app = express()
 app.use(express.static(path.resolve(__dirname,'public')))
 // app.get('/',(req,res)=>{
 //     res.status(200).sendfile('./expressjs/public/wc1.html')
 // })
+app.use(express.json())
+const products = [
+    {name:"laptop",price:200.0},
+    {name:"phone",price:100.0},
+    {name:"tv",price:150.0}
+]
 
+// feature to add products to a shop
+app.get("/",(req,res)=>{
+    const {name,price} =req.body
+    if(!name||!price){
+        return res.status(400).json({"message":"please provide a name/price or both"})
+    }
+    products.push({name,price})
+    return res.status(201).json({"message":"product added"})
+
+})
 app.all('*',(req,res)=>{
     res.status(404).send('<h1>not found</h1>')
 })
