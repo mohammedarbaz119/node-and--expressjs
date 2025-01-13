3f7a45be347eb279b48127dd0c89af7cf3c9e39a
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2025-01-13 21:38:43 +0530

    fix: fixed json api

--- expressjs/jsonbascis.js
+++ expressjs/jsonbascis.js
@@ -5,7 +5,7 @@ const {products} = require('./json.js')
 app.get('/',(req,res)=>{
     res.send('<h1>home page</h1> <a href="/json">products</a>')
 })
-app.get('/json:id',(req,res)=>{
+app.get('/json',(req,res)=>{
 res.json(products.map(({name})=>name))
 })
 app.listen(4000,()=>(console.log(400)))
